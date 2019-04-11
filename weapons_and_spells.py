c8ee1aa8db313782b4e192294284fd8877b679c9
nurxx <ilknurmustafa99@gmail.com>
2019-04-11 12:40:15 +0300

    Add dunder __eq__ :heavy_plus_sign:

--- weapons_and_spells.py
+++ weapons_and_spells.py
@@ -6,6 +6,9 @@ class Weapon:
     def __str__(self):
         return '{0} - (damage {1})'.format(self.name,self.damage)
 
+    def __eq__(self,other):
+        return self.__dict__ == other.__dict__
+
 class Spell:
     def __init__(self,name,damage,mana_cost,cast_range):
         self.name = name
@@ -15,3 +18,6 @@ class Spell:
 
     def __str__(self):
         return '{0} - (damage {1}, mana_cost {2}, cast_range {3})'.format(self.name,self.damage,self.mana_cost,self.cast_range)
+
+    def __eq__(self,other):
+        return self.__dict__ == other.__dict__
